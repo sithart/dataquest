dc3624defad064e66484eb9c5666e6654009e774
Sitharthan Chandrasekaran <sitharth@localhost.localdomain>
2020-05-01 23:45:34 +0530

    simple csv read

---
+++ read_csv.py
@@ -0,0 +1,7 @@
+from csv import reader
+opened_file = open('AppleStore.csv')
+read_file = reader(opened_file)
+apps_data = list(read_file)
+print(len(apps_data))
+print(apps_data[0])
+print(apps_data[1:3])
