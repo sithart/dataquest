b45a13cf687b2cabcb1c69f636026d8dd55c4a6b
Sitharthan Chandrasekaran <sitharth@localhost.localdomain>
2020-05-30 16:46:19 +0530

    conditionals & dict mission finished

---
+++ dictionaries_and_frequency_tables/finding_the_unique_values.py
@@ -0,0 +1,12 @@
+from csv import reader
+opened_file = open('AppleStore.csv')
+read_file = reader(opened_file)
+apps_data = list(read_file)
+content_ratings = {}
+for row in apps_data[1:]:
+    c_rating = row[10]
+    if c_rating in content_ratings:
+        content_ratings[c_rating] += 1
+    else:
+        content_ratings[c_rating] = 1
+print(content_ratings)
